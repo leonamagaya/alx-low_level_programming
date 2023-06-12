f17fa1817e63185ec15dc2988d277e23b81a8c8c
leonamagaya <leonamagaya@gmail.com>
2023-06-12 03:33:32 -0700

    handling empty strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,10 +15,10 @@ char *str_concat(char *s1, char *s2)
 	char *new_str;
 
 	if (s1 == NULL)
-		s1[0] = '\0';
+		s1 = "";
 
 	if (s2 == NULL)
-		s2[0] = '\0';
+		s2 = "";
 
 	while (s1[len1] != '\0')
 		len1++;
