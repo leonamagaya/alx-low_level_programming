83b3bad153d242c44077608eda9be9f5a7bd15e3
leonamagaya <leonamagaya@gmail.com>
2023-06-12 05:42:59 -0700

    adjustment of allocated memory

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,9 +21,10 @@ int **alloc_grid(int width, int height)
 
 	if (grid == NULL)
 		return (NULL);
+
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = (int *)malloc(height * sizeof(int));
+		grid[i] = (int *)malloc(width * sizeof(int));
 
 		if (grid[i] == NULL)
 		{
