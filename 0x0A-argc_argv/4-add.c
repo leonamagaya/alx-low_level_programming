e7ff0650e9697a778f2ab379f19a60ae545db0e7
leonamagaya <leonamagaya@gmail.com>
2023-06-13 02:28:20 -0700

    4-add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _atoi - A function that converts strings
+ *         to integers.
+ *
+ * @str: input string.
+ *
+ * Return: integer that was once string.
+ */
+int _atoi(char *str)
+{
+	int result = 0, i = 0;
+	int sign = 1;
+
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+
+	while (str[i] != '\0')
+	{
+		if (str[i] < '0' || str[i] > '9')
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+
+	return (result * sign);
+}
+
+int main(int argc, char **argv)
+{
+	int i, number, sum = 0;
+
+	if (argc == 1)
+		printf("0\n");
+
+	for (i = 1; i < argc; i++)
+	{
+		number = _atoi(argv[i]);
+
+		if (number == 1)
+			return (1);
+
+		sum += number;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
