7f3c52e63544fd364481bf3d9ed93b4dbfddd8c3
leonamagaya <leonamagaya@gmail.com>
2023-06-19 04:34:35 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - record for a dog.
+ * @name: the name of the dog.
+ * @age: the age og the dog.
+ * @owner: the owner of the dog.
+ *
+ * Description: the structure contains all
+ *              data about a dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
