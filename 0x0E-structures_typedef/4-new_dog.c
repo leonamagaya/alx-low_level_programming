f4fb15dacd2d3563f7c6b04e89af915bd9ce3522
leonamagaya <leonamagaya@gmail.com>
2023-06-20 11:10:45 -0700

    correction of 4th project

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,6 +15,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 	int len_name = 0, len_owner = 0, i;
+	char *duplicate_name, *duplicate_owner;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
@@ -26,30 +27,29 @@ dog_t *new_dog(char *name, float age, char *owner)
 	while (name[len_name] != '\0')
 		len_name++;
 
-	new_dog->name = (char *)malloc(sizeof(char) * (len_name + 1));
+	duplicate_name = (char *)malloc(sizeof(char) * (len_name + 1));
 
 	while (name[len_owner] != '\0')
 		len_owner++;
 
-	new_dog->owner = (char *)malloc(sizeof(char) * (len_owner + 1));
+	duplicate_owner = (char *)malloc(sizeof(char) * (len_owner + 1));
 
-	if (new_dog->name == NULL || new_dog->owner == NULL)
+	if (duplicate_name == NULL || duplicate_owner == NULL)
 	{
-		free(new_dog->name);
-		free(new_dog->owner);
-		free(new_dog);
 		return (NULL);
 	}
 
-	for (i = 0; i <= len_name; i++)
-		new_dog->name[i] = name[i];
-	new_dog->name[len_name + 1] = '\0';
+	for (i = 0; i < len_name; i++)
+		duplicate_name[i] = name[i];
+	duplicate_name[len_name + 1] = '\0';
 
 	for (i = 0; i <= len_owner; i++)
-		new_dog->owner[i] = owner[i];
-	new_dog->owner[len_owner + 1] = '\0';
+		duplicate_owner[i] = owner[i];
+	duplicate_owner[len_owner + 1] = '\0';
 
+	new_dog->name = duplicate_name;
 	new_dog->age = age;
+	new_dog->owner = duplicate_owner;
 
 	return (new_dog);
 }
