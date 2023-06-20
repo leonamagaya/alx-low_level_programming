e4900b1937b2723c1a10094a6cda610d8e399d4e
leonamagaya <leonamagaya@gmail.com>
2023-06-20 05:08:38 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -43,11 +43,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	for (i = 0; i <= len_name; i++)
 		new_dog->name[i] = name[i];
-	new_dog->name[len_name] = '\0';
+	new_dog->name[len_name + 1] = '\0';
 
 	for (i = 0; i <= len_owner; i++)
 		new_dog->owner[i] = owner[i];
-	new_dog->owner[len_owner] = '\0';
+	new_dog->owner[len_owner + 1] = '\0';
 
 	new_dog->age = age;
 
