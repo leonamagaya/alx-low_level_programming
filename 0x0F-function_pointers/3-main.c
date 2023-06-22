f9f5c2974924fdb1945df9c5ccd89c11fc234659
leonamagaya <leonamagaya@gmail.com>
2023-06-22 13:46:21 -0700

    a program that performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main function for program.
+ *
+ * @argc: number of arguments.
+ * @argv: array of arguments.
+ *
+ * Return: if success 0
+ *         else 98, 99, 100.
+ */
+int main(int argc, char **argv)
+{
+	int  (*function)(int, int);
+	int a, b;
+
+	if (argc != 4)
+	{
+		prinf("Error\n");
+		return (98);
+	}
+	
+	function = get_op_func(argv[2]);
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	if (function == NULL)
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	printf("%d\n", function(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,65 @@
+#include "calc.h"
+/**
+ * op_add - returns the sum of integers.
+ *
+ * @a: input integer.
+ * @b: input integer.
+ *
+ * Return: sum of a and b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns the difference of integers.
+ *
+ * @a: input integer.
+ * @b: input integer.
+ *
+ * Return: difference of a and b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns the product of integers.
+ *
+ * @a: input integer.
+ * @b: input integer.
+ *
+ * Return: product of a and b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns the result of the division of integers.
+ *
+ * @a: input integer.
+ * @b: input integer.
+ *
+ * Return: quotient of dividing a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod -  returns the remainder of the division of integers.
+ *
+ * @a: input integer.
+ * @b: input integer.
+ *
+ * Return: remainder of the division of a and b.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
