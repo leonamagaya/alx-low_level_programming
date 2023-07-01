387f9a4d849aabf3edf176a6c94b1f43bf292c2d
leonamagaya <leonamagaya@gmail.com>
2023-07-01 09:49:32 -0700

    print list correction

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,24 @@
 #ifndef _LISTS_H
 #define _LISTS_H
 
+#include <stdlib.h>
+
 int _putchar(char c);
+
+/**
+ * struct singly_list - singly linked list.
+ * @str: string within node.
+ * @len: length of the string.
+ * @next: the next node.
+ * Description - singly linked list.
+ */
+typedef struct singly_list
+{
+	char *str;
+	unsigned int len;
+	struct singly_list *next;
+}list_t;
+
 size_t print_list(const list_t *h);
 
 #endif
