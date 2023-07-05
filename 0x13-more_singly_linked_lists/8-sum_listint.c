912b6f508c4e2401773557124e5655b2cbf58013
leonamagaya <leonamagaya@gmail.com>
2023-07-05 16:10:54 -0700

    addressing doc style errors

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -3,7 +3,7 @@
  * sum_listint - a function that returns the sum of all the
  *               data (n) of a listint_t linked list.
  * @head: pointer to a struct.
- * Return: the sum of all data 
+ * Return: the sum of all data
  *         else 0 is list is empty.
  */
 int sum_listint(listint_t *head)
