44e94f06b20ff2d70bde6b3cc3b0f36d07e40b1c
leonamagaya <leonamagaya@gmail.com>
2023-07-05 14:18:33 -0700

    free list int

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - a function that frees a list_t list.
+ *  @head: pointer to a struct.
+ *  Return: nothing.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *next;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		free(head->data);
+		free(head);
+		head = next;
+	}
+}
