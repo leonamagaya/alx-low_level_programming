cf6561d38608815ae8752937b8921bee4b35752f
leonamagaya <leonamagaya@gmail.com>
2023-07-07 08:24:15 -0700

    0.0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,29 +9,24 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
-	bool valid_binary = true;
+	unsigned int result = 0, base = 1, length = 0;
 
 	if (b == NULL)
 		return (0);
 
-	for (int i = 0; b[i] != '\0'; i++)
-	{
-		if (b[i] == '0')
-			result <<= 1;
-		else if (b[i] == '1')
-		{
-			result <<= 1;
-			result |= 1;
-		}
-		else
-		{
-			valid_binary = false;
-			break;
-		}
-	}
-	if (!valid_binary)
-		return (0);
+	while (b[length])
+		length++;
 
+	while (length)
+	{
+		if (b[length - 1] != '0' && b[length - 1] != '1')
+			return (0);
+
+		if (b[length - 1] == '1')
+			result += base;
+		base *= 2;
+		length--;
+	}
 	return (result);
 }
+
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,8 @@
 #define _MAIN_H
 
 #include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
