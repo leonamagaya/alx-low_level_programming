c79cf2cecf0ce8ea672a073f7dcdfef15a3c65c2
leonamagaya <leonamagaya@gmail.com>
2023-07-10 14:47:09 -0700

    create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,13 +13,13 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-		return (-1);
-
-	while (*text_content)
+	if (text_content != NULL)
 	{
-		len++;
-		text_content++;
+		while (*text_content)
+		{
+			len++;
+			text_content++;
+		}
 	}
 
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
