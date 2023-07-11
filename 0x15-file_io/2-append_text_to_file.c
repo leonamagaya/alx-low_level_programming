cffe5029434a4b392714e7b2b94b1a2944ad983c
leonamagaya <leonamagaya@gmail.com>
2023-07-11 05:25:51 -0700

    addressing doc style errors

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -33,8 +33,8 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	file = open(filename, O_RDWR | O_APPEND);
-        if (file == -1)
-                return (-1);
+	if (file == -1)
+		return (-1);
 
 	if (text_content == NULL)
 	{
