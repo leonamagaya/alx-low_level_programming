b4e31e4d3ec70bec791f1f947b3104805556e579
leonamagaya <leonamagaya@gmail.com>
2023-07-11 05:11:37 -0700

    append corrections

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,25 +27,19 @@ int _strlen(char *s)
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file, fd_r, len;
+	int file, fd_w, len;
 
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_WRONLY | O_APPEND);
+	file = open(filename, O_RDWR | O_APPEND);
 	if (file == -1)
 		return (-1);
 
-	if (text_content == NULL)
-	{
-		close(file);
-                return (-1);
-	}
-
 	len = _strlen(text_content);
-	fd_r = write(file, text_content, len);
+	fd_w = write(file, text_content, len);
 
-	if (fd_r == -1 || fd_r != len)
+	if (fd_w == -1 || text_content == NULL)
 	{
 		close(file);
 		return (-1);
