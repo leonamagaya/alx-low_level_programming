4a08be5c8f43a61f119884b0723950b7a073a41f
leonamagaya <leonamagaya@gmail.com>
2023-07-26 12:06:37 -0700

    4 clear bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,6 +10,9 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int max = 0x01;
+	
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
 
 	max = ~(max << index);
 	if (max == 0x00)
