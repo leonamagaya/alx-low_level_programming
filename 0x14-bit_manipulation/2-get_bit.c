0e521e8983b8e068845f1faa74cab42c2ee6d4f3
leonamagaya <leonamagaya@gmail.com>
2023-07-26 11:21:06 -0700

    corrections

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,6 +11,9 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int max = 0x01;
 
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
 	max <<= index;
 	if (max == 0)
 		return (-1);
