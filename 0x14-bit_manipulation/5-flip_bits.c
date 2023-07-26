ea795832c80e8644eea10adcf49d6bc10f09988b
leonamagaya <leonamagaya@gmail.com>
2023-07-26 12:25:53 -0700

    trailing whitespaces

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,12 +12,12 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int i = 0;
 	unsigned long int result = n ^ m;
-	
+
 	while (result)
 	{
 		i += result & 1;
 		result >>= 1;
 	}
-	
+
 	return (i);
 }
