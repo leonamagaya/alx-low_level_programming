ff8ddd2e6217f7050b44cb4642cbbdc812da9f4e
leonamagaya <leonamagaya@gmail.com>
2023-07-26 11:58:06 -0700

    doc style error

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * set_bit - a function that sets the value of a bit 
+ * set_bit - a function that sets the value of a bit
  *           to 1 at a given index
  * @index:  is the index, starting from 0
  * @n: pointer to value.
@@ -12,6 +12,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index > 63)
 		return (-1);
 
-	*n = ((1UL << index)|*n);
+	*n = ((1UL << index) | *n);
 	return (1);
 }
