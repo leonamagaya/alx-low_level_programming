96763090b90dd6519e9b05cd032da57b0e41e779
leonamagaya <leonamagaya@gmail.com>
2023-08-03 06:15:58 -0700

    trailing white spaces

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_binary - a function that prints the 
+ * print_binary - a function that prints the
  *                binary representation of a number
  * @n: number to be converted.
  * Return: nothing.
@@ -11,5 +11,5 @@ void print_binary(unsigned long int n)
 	if (n > 1)
 		print_binary(n >> 1);
 
-	_putchar((n | 1) + '0');
+	_putchar((n & 1) + '0');
 }
