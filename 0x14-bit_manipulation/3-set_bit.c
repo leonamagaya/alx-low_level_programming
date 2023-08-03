820bd3dd72370bad4849d821ac798011238e7b8d
leonamagaya <leonamagaya@gmail.com>
2023-08-03 07:14:06 -0700

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,15 +11,15 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int max_bits;
+	unsigned long int mask;
 
 	max_bits = sizeof(unsigned long int) * 8;
 
 	if (n == NULL || index >= max_bits)
 		return (-1);
 
-	max_bits <<= index;
-	if (max_bits == 0)
-		return (-1);
-	*n |= max_bits;
+	mask = 1UL << index;
+	*n |= mask;
+
 	return (1);
 }
