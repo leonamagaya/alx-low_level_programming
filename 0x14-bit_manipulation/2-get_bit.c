f8d82f5a3c8f7005ea613bbfaea6206f7a9da12c
leonamagaya <leonamagaya@gmail.com>
2023-08-03 06:49:19 -0700

    doc-style

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,6 +5,7 @@
  *          of a bit at a given index.
  * @index:  the index, starting from 0 of the bit
  *          you want to get.
+ * @n: number to get bit from.
  * Return: the value of the bit at index index or
  *         -1 if an error occured.
  */
